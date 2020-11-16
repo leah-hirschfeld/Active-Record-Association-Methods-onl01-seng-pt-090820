e765d6814d16973709c444f2bb9008973466dfec
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-16 22:26:46 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,6 +11,6 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.songs.artist.name# return an array of strings containing every musician's name
+    self.song.artist.name# return an array of strings containing every musician's name
   end
 end
