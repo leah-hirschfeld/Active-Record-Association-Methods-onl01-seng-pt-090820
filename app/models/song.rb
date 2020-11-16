6cc14a47c7242442e6457650abbb34813ed6583c
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-16 23:32:31 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    if Artist.include? "Drake"
+    if Artist.name.include? "Drake"
       self.artist == "Drake"
     else
       drake = Artist.new(name: "Drake")
