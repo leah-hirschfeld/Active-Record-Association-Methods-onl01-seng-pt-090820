234e4cafceacaab49848bfbe20c6ab0e84f76f48
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-16 23:19:01 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,7 +10,7 @@ class Song < ActiveRecord::Base
   def drake_made_this
     if self.artist == drake
       self.artist = drake
-      self.artist != "Drake"
+    else self.artist != "Drake"
       drake = Artist.new(name: "Drake")
       self.artist = drake
       drake.save
